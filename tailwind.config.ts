1d4144c62d0e981077d7ab9d24b61443302bec22
Chip-Mortensen <39780632+Chip-Mortensen@users.noreply.github.com>
2025-01-21 10:01:37 -0700

    removing js tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,9 +7,24 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            a: {
+              color: '#2563eb',
+              '&:hover': {
+                color: '#1d4ed8',
+              },
+            },
+            color: '#374151',
+          },
+        },
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
   ],
 } satisfies Config;
